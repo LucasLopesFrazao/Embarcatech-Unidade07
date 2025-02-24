3e051218f892b844cb78a63915abad6fd07e301d
LucasLopesFrazao <lucaslopesfrazao2003@gmail.com>
2025-02-23 21:20:25 -0300

    change

--- build/CMakeFiles/microphone_dma_ws2818b_pio_h.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for microphone_dma_ws2818b_pio_h.
-# This may be replaced when dependencies are built.
--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/lucasfrazao/Documentos/BitDogLab-C/microphone_dma/build/_deps/picotool/"
-};
